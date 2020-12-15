7752d0207bc69b12e1c2e8dc101263b7f6ca2093
Jason <jasondao415@gmail.com>
2020-12-15 02:59:32 -0500

    Fixed URL 4

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app=express();            //create app using express
 app.use(express.static('public'));//use public folder for html etc
 
 //SERVER
-const server=app.listen( process.env.PORT || 4000, function() {          //listen to port 4000, and if open, prints function, and allows connection to website
-    console.log("Server started on port 4000")
+const server=app.listen(process.env.PORT ||4000, () => {
+    console.log("Server started on port 4000");
 })
 
 
